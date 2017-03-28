0d8efc0330d2ed43a578c50b4c3bc31ed7d69b29
Anthony Martinet <anthony@alkivi.fr>
2017-03-28 10:44:26 +0200

    fix pypi name

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except(IOError, ImportError):
     long_description = open(readme).read()
 
 setup(
-    name='alkivi-google',
+    name='alkivi-google-client',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
